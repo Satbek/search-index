2a96ef454dd20efa872a1442b4a4a1e1a27fcc28
Satbek Turganbayev <s.turganbayev@corp.mail.ru>
2021-10-27 11:30:17 +0300

    Initial commit

---
+++ app/roles/custom.lua
@@ -0,0 +1,42 @@
+local cartridge = require('cartridge')
+
+local function init(opts) -- luacheck: no unused args
+    -- if opts.is_master then
+    -- end
+
+    local httpd = assert(cartridge.service_get('httpd'), "Failed to get httpd serivce")
+    httpd:route({method = 'GET', path = '/hello'}, function()
+        return {body = 'Hello world!'}
+    end)
+
+    local http_handler = require('metrics.plugins.prometheus').collect_http
+    httpd:route({path = '/metrics'}, function(...)
+        return http_handler(...)
+    end)
+
+    return true
+end
+
+local function stop()
+    return true
+end
+
+local function validate_config(conf_new, conf_old) -- luacheck: no unused args
+    return true
+end
+
+local function apply_config(conf, opts) -- luacheck: no unused args
+    -- if opts.is_master then
+    -- end
+
+    return true
+end
+
+return {
+    role_name = 'app.roles.custom',
+    init = init,
+    stop = stop,
+    validate_config = validate_config,
+    apply_config = apply_config,
+    -- dependencies = {'cartridge.roles.vshard-router'},
+}
